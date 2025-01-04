9c77406b8d65cb9ca3a319a14b4c239db17e33a8
Vladislav-Tsekov-y <vladislavcekov2003@gmail.com>
2025-01-04 22:29:58 +0200

    Initial commit with Spring Boot project

---
+++ src/main/java/com/ISN3Java/ISN3Java/Isn3JavaApplication.java
@@ -0,0 +1,13 @@
+package com.ISN3Java.ISN3Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Isn3JavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Isn3JavaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ISN3Java/ISN3Java/Isn3JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ISN3Java.ISN3Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Isn3JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
